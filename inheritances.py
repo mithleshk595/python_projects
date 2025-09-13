a22719e026ec73220ca713d90300be89783f84be
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-13 18:29:13 +0530

    Added inheritances.py

---
+++ inheritances.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractclassmethod
+class character(ABC):
+    @abstractclassmethod
+    def patriotism(self):
+        pass
+
+class Actor:
+    def style(self):
+        print(">> Actor.style:")
+
+
+class person(Actor, character):
+    def do_acting(self):
+        print(">> person.doActing")
+    
+    def style(self):
+        print(">> person.style")
+    
+    def patriotism(self):
+        print(">> person.patriotism")
+    
+p = person()
+p.patriotism()
+p.style()
+p.do_acting()
