d7d43a6aa629788e789f09f2c66b37bf8a92aa7b
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-14 11:07:43 +0530

    Added generators.py

---
+++ generators.py
@@ -0,0 +1,13 @@
+import math 
+pi = 3.14
+sine_table = {ang : math.sin(ang * pi / 180) for ang in range(0, 360, 90)}
+cos_table = {ang : math.cos(ang * pi /180) for ang in range(0, 360, 90)}
+tan_table = {ang : math.tan(ang * pi / 180) for ang in range(0, 360, 90) }
+print(sine_table)
+print(cos_table)
+print(tan_table)
+
+maxsine = max((math.sin(ang * pi / 180) for ang in range(0, 360, 90)))
+maxcos = max((math.cos(ang * pi / 180) for ang in range(0, 360, 90)))
+print(maxsine)
+print(maxcos)
--- iterators.py
+++ iterators.py
@@ -1,30 +1,31 @@
-# def generate_primes():
-#     num = 1
-#     while True:
-#         if isprime(num):
-#             yield num
-#         num += 1
+def generate_primes():
+    num = 1
+    while True:
+        if isprime(num):
+            yield num
+        num += 1
 
-# def isprime(n):
-#     if n >  1:
-#         if n == 2:
-#             return False
-#         if n % 2 == 0:
-#             return False
-#         for i in range(2, n // 2):
-#             if n % i == 0:
-#                 return False
+def isprime(n):
+    if n >  1:
+        if n == 2:
+            return False
+        if n % 2 == 0:
+            return False
+        for i in range(2, n // 2):
+            if n % i == 0:
+                return False
         
-#         else:
-#             return False
+        else:
+            return False
         
-# total = 0
-# for next_prime in generate_primes():
-#     if next_prime < 300000:
-#         total += next_prime
-#     else:
-#         print(total)
-#         exit()
+total = 0
+for next_prime in generate_primes():
+    if next_prime < 300000:
+        total += next_prime
+    else:
+        print(total)
+        exit()
+
 
 
 
