916008fc8fe4365ed4973ee4af9e203f2155c6df
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-17 21:29:52 +0530

    Added maths.py

---
+++ maths.py
@@ -0,0 +1,31 @@
+# x = min(5, 10, 15)
+# y = min(5, 10, 15)
+
+
+
+# print(x)
+# print(y)
+
+
+
+# x = abs(-7.25)
+# print(x)
+
+
+
+# x = pow(4, 3)
+# print(x)
+
+
+
+# import math
+# x = math.sqrt(64)
+# print(x)
+
+
+import math
+x = math.ceil(1.4)
+y = math.ceil(1.4)
+
+print(x)
+print(y)
