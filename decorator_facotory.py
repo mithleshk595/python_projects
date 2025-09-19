9ac85fdc426d7e91a096d05d46ae589be241270e
Mithleshk595 <mithleshk0982@gmail.com>
2025-09-19 08:47:14 +0530

    Added decotators_factory.py

---
+++ decorator_facotory.py
@@ -0,0 +1,20 @@
+def changecase(n):
+    def changecase(func):
+        def myinner():
+            if n == 1:
+                a = func().lower()
+            else:
+                a = func().upper()
+            return a
+        return myinner
+    return changecase
+
+@changecase(1)
+def myfunction():
+    return "Hello linus"
+
+print(myfunction())
+
+
+
+
